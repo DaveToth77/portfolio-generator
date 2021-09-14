89d5b730c3562937d0a14addbb2e23ba679390d8
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-09-14 13:35:32 -0400

    added inquirer npm

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-console.log(profileDataArgs);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name:  'name',
+        messaage: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
